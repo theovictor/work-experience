c15f04b04f841f5a3ae70f158cab5aa2d010c252
Theo <teo.pvh@gmail.com>
2021-11-28 17:17:57 -0400

    edit navbar css

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,22 +6,24 @@ export function MainNavbar() {
 
   return (
     <>
-      <Navbar collapseOnSelect expand="sm" fixed="top" variant="dark">
-        <Container>
-          <Navbar.Brand>Work Experience</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="m-auto">
-              <NavLink to="/home" className={isActive => styles.active + (!isActive ? "unselected" : '')}>Início</NavLink>
-              <NavLink to="/about" className={isActive => styles.active + (!isActive ? "unselected" : '')}>Sobre nós</NavLink>
-              <NavLink to="/contact" className={isActive => styles.active + (!isActive ? "unselected" : '')}>Contato</NavLink>
-            </Nav>
-            <Nav>
-              <Button className={`${styles.btn} bg-white text-dark`}>Entrar</Button>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div className={styles.navContent}>
+        <Navbar collapseOnSelect expand="lg">
+          <Container fluid>
+            <Navbar.Brand>Work Experience</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="m-auto">
+                <NavLink to="/home" className={isActive => `ml-2 ${styles.active}` + (!isActive ? "unselected" : '')}>Início</NavLink>
+                <NavLink to="/about" className={isActive => styles.active + (!isActive ? "unselected" : '')}>Sobre nós</NavLink>
+                <NavLink to="/contact" className={isActive => styles.active + (!isActive ? "unselected" : '')}>Contato</NavLink>
+              </Nav>
+              <Nav>
+                <Button className={`${styles.btn} bg-white text-dark`}>Entrar</Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     </>
   );
 }
