eed2886ca552a2fc1db775642644e89225dbcd1a
Theo <teo.pvh@gmail.com>
2021-11-28 15:48:37 -0400

    navbar finalizada

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,22 +1,23 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
 import styles from "./styles.module.scss";
 
 export function MainNavbar() {
+
   return (
     <>
-      <Navbar collapseOnSelect expand="sm" fixed="top" variant="dark" className={styles.active}>
+      <Navbar collapseOnSelect expand="sm" fixed="top" variant="dark">
         <Container>
           <Navbar.Brand>Work Experience</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="m-auto">
-              <Link to="/home">Início</Link>
-              <Link to="/about">Sobre nós</Link>
-              <Link to="/contact">Contato</Link>
+              <NavLink to="/home" className={isActive => styles.active + (!isActive ? "unselected" : '')}>Início</NavLink>
+              <NavLink to="/about" className={isActive => styles.active + (!isActive ? "unselected" : '')}>Sobre nós</NavLink>
+              <NavLink to="/contact" className={isActive => styles.active + (!isActive ? "unselected" : '')}>Contato</NavLink>
             </Nav>
             <Nav>
-              <Link to="/home">More deets</Link>
+              <Button className={`${styles.btn} bg-white text-dark`}>Entrar</Button>
             </Nav>
           </Navbar.Collapse>
         </Container>
