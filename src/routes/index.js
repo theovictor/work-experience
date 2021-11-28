aadc305b4a38e4bcedf8641afa664a6147c71991
Theo <teo.pvh@gmail.com>
2021-11-28 15:23:09 -0400

    edit routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,21 @@
-import { Switch } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Switch, useHistory, useLocation } from 'react-router-dom';
 import { RouteWithLayout } from './RouteWithLayout';
-
 import MainLayout from '../layout/Main';
 import HomePage from '../pages/Home/Home';
 import AboutPage from '../pages/About/About';
 import ContactPage from '../pages/Contact/Contact';
 
 export default function Index() {
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      history.push('/home')
+    }
+  }, [location])
+
   return (
     <Switch>
       <RouteWithLayout exact component={HomePage} layout={MainLayout} path="/home"/>
